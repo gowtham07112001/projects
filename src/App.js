56dde8cee8b8544c5af8cf713caff7afd1315ce3
gowtham07112001 <gowtham07112001@gmail.com>
2023-06-04 11:21:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import './Components/NavbarStyle.css';
-import Home from './routes/Home';
-import About from './routes/About';
-import Service from './routes/Service';
-import Contact from './routes/Contact';
-import { Routes, Route } from 'react-router-dom';
+import "./Components/NavbarStyle.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/service' element={<Service />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-
-
-      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
     </div>
   );
 }
